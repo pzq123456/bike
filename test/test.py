d24c779b4cfc352f78d8f4c63d3959f8cd9c2885
panzhiqing <1812673119@qq.com>
2024-04-30 18:34:36 +0800

    Update shared bike research findings and references in c2.md and c3.md

--- test/test.py
+++ test/test.py
@@ -1,12 +1,45 @@
-# 计算每一个小时 占当天总数的比例
-# frequency = frequency / frequency.sum(axis=1)[:, None]
+import math
+from typing import List
+import time
 
-# 测试 计算每一个小时 占当天总数的比例 是否正确
-import numpy as np
+def haversine(latlng1: List[float], latlng2: List[float], R: float = 6378137.0) -> float:
+    rad = math.pi / 180
+    lat1 = latlng1[0] * rad
+    lat2 = latlng2[0] * rad
+    sinDLat = math.sin((latlng2[0] - latlng1[0]) * rad / 2)
+    sinDLon = math.sin((latlng2[1] - latlng1[1]) * rad / 2)
+    a = sinDLat * sinDLat + math.cos(lat1) * math.cos(lat2) * sinDLon * sinDLon
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    return R * c
 
-frequency = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-print(frequency)
-print(frequency.sum(axis=1))
-print(frequency.sum(axis=1)[:, None])
-print(frequency / frequency.sum(axis=1)[:, None])
+# 秒 的单位转换
+def time_convert(seconds: float) -> str:
+    if seconds < 1e-6:
+        return f"{seconds * 1e9:.2f} ns"
+    if seconds < 1e-3:
+        return f"{seconds * 1e6:.2f} μs"
+    if seconds < 1:
+        return f"{seconds * 1e3:.2f} ms"
+    return f"{seconds:.2f} s"
 
+# 实现一个量测函数运行时间的装饰器
+def measure_time(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        # print(f"Function {func.__name__} took {end - start} seconds")
+        print(f"Function {func.__name__} took {time_convert(end - start)}")
+        return result
+    return wrapper
+
+
+
+
+if __name__ == "__main__":
+    # 测试装饰器
+    @measure_time
+    def test():
+        haversine([31.22, 121.48], [39.93, 116.40])
+
+    test()
