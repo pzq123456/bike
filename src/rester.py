86b54a2fb3577c6db676f52c12d5e2fd15fd0ae8
潘志清 <82391775+pzq123456@users.noreply.github.com>
2024-05-11 20:24:28 +0800

    feat: 优化共享单车轨迹数据处理

--- src/rester.py
+++ src/rester.py
@@ -91,6 +91,17 @@ def coverage(raster1, raster2):
     coverage = np.sum(intersection) / np.sum(data1)
     return coverage
 
+def coverage_(data1, data2):
+    # 二值化
+    data1[data1 != 0] = 1
+    data2[data2 != 0] = 1
+    # 计算两个栅格数据的交集
+    intersection = np.logical_and(data1, data2)
+    # 计算道路对通勤轨迹的覆盖率
+    coverage = np.sum(intersection) / np.sum(data1)
+    return coverage
+
+
 # IoU 计算
 def iou(raster1, raster2):
     data1, geotransform, projection = read_single_band_image(raster1)
@@ -148,46 +159,106 @@ def gradient(data):
     data = cv2.morphologyEx(data, cv2.MORPH_GRADIENT, kernel)
     return data
 
+# 去除点状噪声
+def remove_noise(data, size=3):
+    data = cv2.medianBlur(data, size) 
+    return data
 
+def helper(data, mykernel):
+    data = close(data)
+    data = erode(data, mykernel)
+    return data
 
+# 绘制轨迹数据 需要绘制颜色条带 并对数据作夸张处理
+def draw_track():
+    data, geotransform, projection = read_single_band_image('H:\\bike\qgis\轨迹.tif')
+    # 设置拉伸函数
+    data = np.power(data, 0.5)
+    # 获取数据的最大值和最小值
+    min_value = np.min(data)
+    max_value = np.max(data)
+    # 设置数据的最大值和最小值
+    vmin = min_value
+    vmax = max_value
+    # 根据最大值最小值划分 3 个层级 并提取每一个层级的数据 空位 0
+    data1 = np.where(data < (max_value - min_value) / 3, data, 0)
+    data2 = np.where(np.logical_and(data >= (max_value - min_value) / 3, data < 2 * (max_value - min_value) / 3), data, 0)
+    data3 = np.where(data >= 2 * (max_value - min_value) / 3, data, 0)
+    # 绘制轨迹数据
+    fig, axs = plt.subplots(1, 3, figsize=(15, 5))
+    # 绘制第一个层级的数据
+    ax = axs[0]
+    # 对 data1 做腐蚀
+    data1 = close(data1)
+    im = ax.imshow(data1, cmap='hot', vmin=vmin, vmax=vmax)
+    ax.set_title('{:.2f} < desity < {:.2f}'.format(min_value, (max_value - min_value) / 3))
+    ax.axis('off')
+    # 绘制第二个层级的数据
+    ax = axs[1]
+    im = ax.imshow(data2, cmap='hot', vmin=vmin, vmax=vmax)
+    ax.set_title('{:.2f} < desity < {:.2f}'.format((max_value - min_value) / 3, 2 * (max_value - min_value) / 3))
+    ax.axis('off')
+    # 绘制第三个层级的数据
+    ax = axs[2]
+    # 对data3 做膨胀操作
+    data3 = dilate(data3)
+    data3 = remove_noise(data3)
+    # 对data3 做开运算
+    data3 = open(data3)
+    # 对data3 做闭运算
+    data3 = close(data3)
 
-
+    im = ax.imshow(data3, cmap='hot', vmin=vmin, vmax=vmax)
+    ax.set_title('{:.2f} < desity < {:.2f}'.format(2 * (max_value - min_value) / 3, max_value))
+    ax.axis('off')
+    # 添加颜色条带
+    fig.colorbar(im, ax=axs.ravel().tolist(), shrink=0.6)
+    plt.show()
 
 
 if __name__ == '__main__':
+    # draw_track()
     image_paths = ['H:\\bike\qgis\轨迹.tif', 'H:\\bike\qgis\主路.tif', 'H:\\bike\qgis\次主路.tif', 'H:\\bike\qgis\支路.tif']
     names = ['track ∩ primary_road', 'track ∩ secondary_road', 'track ∩ tertiary_road']
-    # 计算轨迹与主路、次主路、支路的覆盖率
-    # coverages = [coverage(image_paths[0], image_paths[i + 1]) for i in range(3)]
 
-    #向 name 中添加覆盖率
-    # for i in range(3):
-    #     names[i] += ' ' + "{:.2%}".format(coverages[i]) + " coverage"
     mykernel = np.array([
         [0,1],
         [0,1]
     ], np.uint8)
+
+    intersection_data = []
 
     fig, axs = plt.subplots(1, 3, figsize=(15, 5))
     for i in range(3):
         diff_data = intersection(image_paths[0], image_paths[i + 1])
 
         # 闭运算
-        diff_data = close(diff_data)
         diff_data = erode(diff_data, mykernel)
 
-     
+        diff_data = dilate(diff_data, mykernel)
+        diff_data = dilate(diff_data, mykernel)
+        diff_data = close(diff_data, mykernel)
+        # 去除噪声
 
+
+        intersection_data.append(diff_data)
+
+    # 计算轨迹与主路、次主路、支路的覆盖率
+    # coverages = [coverage(image_paths[0], image_paths[i + 1]) for i in range(3)]
+    coverages = [coverage_(binaryzation(read_single_band_image(image_paths[0])[0])
+                           , helper(binaryzation(read_single_band_image(image_paths[i + 1])[0]), mykernel)) for i in range(3)]
+
+    # 向 name 中添加覆盖率
+    for i in range(3):
+        names[i] += ' ' + "{:.2%}".format(coverages[i]) + " coverage"
+
+    # 为每个子图设置标题
+    for i in range(3):
         ax = axs[i]
-        ax.imshow(diff_data, cmap='hot')
+        ax.imshow(intersection_data[i], cmap='gray')
         ax.set_title(names[i])
         ax.axis('off')
 
-    # print coverages
-    # for i in range(3):
-    #     print(coverages[i])
-
-    # add bar of the color
     plt.show()
 
 # pip --proxy 127.0.0.1:7890 install --upgrade opencv-python
